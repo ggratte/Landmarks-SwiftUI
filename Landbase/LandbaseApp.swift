4f6f729f338a56244ca0af8349f4ffc8cb0349d8
Göran Gratte <goran.gratte@gmail.com>
2024-02-02 02:17:43 +0100

    Initial Commit

---
+++ Landbase/LandbaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandbaseApp.swift
+//  Landbase
+//
+//  Created by Göran Gratte on 2024-02-02.
+//
+
+import SwiftUI
+
+@main
+struct LandbaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
